98fd8c1d60a289892d6f3e1d7969a56f12ac4e24
Ben Nahill <bnahill@gmail.com>
2020-11-30 22:07:02 -0500

    Major cleanup BREAKING PYTHON 2 COMPATIBILITY:
    - Type annotations used in svd.py, less extensively elsewhere
    - Handling a number of registers that were previously dropped due to
    comments in SVD being mis-interpreted
    - Got rid of split between 2.7 and 3.x dictionary access, favoring the
    3.x approach
    - Tested with ATSAMA5D33.svd, LPC11Exx_v5.svd, M061.svd, MB9AF10xN.svd, SIM3C1x4_SVD.svd, STM32F439x.svd, TM4C1230H6PM.svd, nrf51.svd

--- cmdebug/svd_gdb.py
+++ cmdebug/svd_gdb.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with PyCortexMDebug.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import binascii
 import gdb
 import re
 import math
